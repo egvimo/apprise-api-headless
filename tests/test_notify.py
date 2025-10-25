c58475f4103e876fbe330d9de37f74c2c259f13f
egvimo <egvimo@gmail.com>
2025-10-25 20:00:31 +0000

    test: improve notify tests

--- tests/test_notify.py
+++ tests/test_notify.py
@@ -1,29 +1,40 @@
-from http.server import HTTPServer
 from pathlib import Path
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest_httpserver import HTTPServer
 
 from apprise_api_headless.main import app
 
 
+def config(host: str, port: int) -> str:
+    return (
+        f"urls:\n"
+        f"- 'json://{host}:{port}/tag':\n"
+        f"  - tag: tag\n"
+        f"- 'json://{host}:{port}/no-tag'\n"
+    )
+
+
 @pytest.fixture(autouse=True)
 def apprise_config(httpserver: HTTPServer):
-    httpserver.expect_request("/").respond_with_json({"result": "ok"})
+    httpserver.expect_request("/tag").respond_with_json({"result": "ok"})
+    httpserver.expect_request("/no-tag").respond_with_json({"result": "ok"})
+
     with TestClient(app) as client:
         config_dir = Path(client.app.state.settings.apprise_config_dir)
         config_dir.mkdir(parents=True, exist_ok=True)
-        with open(config_dir / "test-apprise.yml", "w") as file:
-            file.write(
-                f"urls:\n- 'json://{httpserver.host}:{httpserver.port}/':\n  - tag: tag"
-            )
+        with open(config_dir / "test.yml", "w") as file:
+            file.write(config(httpserver.host, httpserver.port))
 
 
-def test_notify_success():
+def test_notify_success(httpserver: HTTPServer):
     with TestClient(app) as client:
-        response = client.post("/notify/test-apprise", json={"body": "body"})
+        response = client.post("/notify/test", json={"body": "body"})
         assert response.status_code == 200
         assert response.json()["success"] is True
+
+    assert len(httpserver.log) == 2
 
 
 def test_notify_missing_config():
@@ -32,45 +43,47 @@ def test_notify_missing_config():
         assert response.status_code == 404
 
 
-def test_notify_with_title():
+def test_notify_with_title(httpserver: HTTPServer):
     with TestClient(app) as client:
-        response = client.post(
-            "/notify/test-apprise", json={"title": "title", "body": "body"}
-        )
+        response = client.post("/notify/test", json={"title": "title", "body": "body"})
         assert response.status_code == 200
         assert response.json()["success"] is True
 
+    assert len(httpserver.log) == 2
 
-def test_notify_with_tag():
+
+def test_notify_with_tag(httpserver: HTTPServer):
     with TestClient(app) as client:
-        response = client.post(
-            "/notify/test-apprise", json={"body": "body", "tag": "tag"}
-        )
+        response = client.post("/notify/test", json={"body": "body", "tag": "tag"})
         assert response.status_code == 200
         assert response.json()["success"] is True
 
+    assert len(httpserver.log) == 1
 
-def test_notify_with_missing_tag():
+
+def test_notify_with_missing_tag(httpserver: HTTPServer):
     with TestClient(app) as client:
-        response = client.post(
-            "/notify/test-apprise", json={"body": "body", "tag": "missing"}
-        )
+        response = client.post("/notify/test", json={"body": "body", "tag": "missing"})
         assert response.status_code == 424
 
+    assert len(httpserver.log) == 0
 
-def test_notify_with_title_and_tag():
+
+def test_notify_with_title_and_tag(httpserver: HTTPServer):
     with TestClient(app) as client:
         response = client.post(
-            "/notify/test-apprise",
+            "/notify/test",
             json={"title": "title", "body": "body", "tag": "tag"},
         )
         assert response.status_code == 200
         assert response.json()["success"] is True
 
+    assert len(httpserver.log) == 1
 
-def test_notify_extra_fields():
+
+def test_notify_extra_fields(httpserver: HTTPServer):
     with TestClient(app) as client:
-        response = client.post(
-            "/notify/test-apprise", json={"body": "body", "extra": "field"}
-        )
+        response = client.post("/notify/test", json={"body": "body", "extra": "field"})
         assert response.status_code == 200
+
+    assert len(httpserver.log) == 2
